83f3e8d5a55e7074fc873bab08fdd7d1147b00e8
Sebastian Maniak <sebastian@maniak.io>
2021-02-18 12:05:23 -0500

    Add files via upload

---
+++ vault/varaibles.tf
@@ -0,0 +1,8 @@
+# --- Vault/variables.tf ---
+variable "instance_type" {}
+variable "instance_count" {}
+variable "vol_size" {}
+variable "public_subnets" {}
+variable "vault_sg" {}
+variable "public_key_path" {}
+variable "key_name" {}
